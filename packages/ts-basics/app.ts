f96b8382246c7310e3f78da0f55c2b5450a790d7
Jemal Khachidze <jkhachidze92@gmail.com>
2026-07-21 17:25:54 +0200

    feat(packages): add working files from TS basic chapter

---
+++ packages/ts-basics/app.ts
@@ -0,0 +1,22 @@
+// // app.ts
+// const shelterName: string = "Sunflower Commons";
+// const catCount: number = 7;
+//
+// function displayShelterInfo(shelterName: string, catCount: number): void {
+//     console.log(`${shelterName} has ${catCount} cats.`);
+// }
+
+// let userName: string = "Tony";
+//
+// console.log(userName.toLowerCase());
+//
+// userName = "25"
+//
+// console.log(userName.toLowerCase());
+
+const shelterName: string = "Sunflower Commons";
+const catCount: number = 7;
+
+function displayShelterInfo (shelterName:string, catCount: number):void {
+  console.log(`${shelterName} has ${catCount} cats.`)
+}
