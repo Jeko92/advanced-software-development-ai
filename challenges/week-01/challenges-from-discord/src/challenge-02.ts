6a69f9265617b8068c56a850348a16380848db38
Jemal Khachidze <jkhachidze92@gmail.com>
2026-07-21 17:29:23 +0200

    feat(packages): add solutions to challenges posted in discord

---
+++ challenges/week-01/challenges-from-discord/src/challenge-02.ts
@@ -0,0 +1,15 @@
+function greet(name: string) : string {
+    return `Hello, ${name}!`;
+}
+
+function logMessage(message: string): void {
+    console.log(message);
+}
+
+function add(a: string, b: string) :string {
+    return a + b;
+}
+
+console.log(greet('Sam'));
+logMessage('Welcome to TypeScript');
+console.log(add('5', '7'));
