f9bcfdb25e2d3889be05a70c13d658c385941bfd
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-25 00:03:54 +0200

    fix(bootcamp:nestjs): switch nestjs-typeorm demo to ts-node for TypeORM CLI compatibility

--- bootcamp/04-nestjs/nestjs-typeorm/demo/src/db/data-source.ts
+++ bootcamp/04-nestjs/nestjs-typeorm/demo/src/db/data-source.ts
@@ -1,7 +1,10 @@
 // src/db/data-source.ts
+import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import { Boardgame } from '../boardgames/entities/boardgame.entity.ts';
 import { Room } from '../rooms/entities/room.entity.ts';
+
+config({ quiet: true });
 
 export const AppDataSource = new DataSource({
   type: 'better-sqlite3',
