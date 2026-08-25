c4fdd8363c09a36a649f046e4669df25a48cea14
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-25 13:48:48 +0200

    feat(bootcamp:nestjs): add nestjs-swagger demo

---
+++ bootcamp/04-nestjs/nestjs-swagger/demo/src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module.ts';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  //  Turns the Response DTO into a plain object
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludeExtraneousValues: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Game Night API')
+    .setDescription('play games with your friends online')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env['PORT'] ?? 3000);
+}
+void bootstrap();
