c0674efa5e4d1e9ea8740122d87846b9a554d434
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-31 10:52:10 +0200

    refactor(bootcamp:devops): simplify calculateLateFee with Math.min

--- bootcamp/05-devops/code-along/src/vanilla/library.ts
+++ bootcamp/05-devops/code-along/src/vanilla/library.ts
@@ -1,7 +1,3 @@
 export const calculateLateFee = (daysOverdue: number): number => {
-  const fee = daysOverdue * 2;
-  if (fee > 10) {
-    return 10;
-  }
-  return fee;
+  return Math.min(daysOverdue * 2, 10);
 };
